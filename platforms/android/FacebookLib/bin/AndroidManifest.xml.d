efa69221370f325b42737fddfb64e3e3a35442e0
Toni Thenhausen <toni@codefuture.de>
2014-09-22 10:54:36 +0200

    Changed SDK Requirements for Facebook

--- platforms/android/FacebookLib/bin/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-D:\Projects\Gutscheinheft\GutscheinheftApp\bld\Release\plugins\facebook\platforms\android\FacebookLib\bin\AndroidManifest.xml : \
-D:\Projects\Gutscheinheft\GutscheinheftApp\bld\Release\plugins\facebook\platforms\android\FacebookLib\AndroidManifest.xml \
