80cdae2a6756194a332e696c548e9fca0f38b205
Toni Thenhausen <toni@codefuture.de>
2014-09-22 10:42:22 +0200

    Added compiled Facebook library

---
+++ platforms/android/FacebookLib/bin/AndroidManifest.xml.d
@@ -0,0 +1,2 @@
+D:\Projects\Gutscheinheft\GutscheinheftApp\bld\Release\plugins\facebook\platforms\android\FacebookLib\bin\AndroidManifest.xml : \
+D:\Projects\Gutscheinheft\GutscheinheftApp\bld\Release\plugins\facebook\platforms\android\FacebookLib\AndroidManifest.xml \
